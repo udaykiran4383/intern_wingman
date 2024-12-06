9155fa2df298971ec75154eadd6892998fdda7ef
uday <uday@udays-MacBook-Air-2.local>
2024-12-06 22:36:26 +0530

    Fix tailwind.config.js for ES module support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Adjust this for your file structure
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
